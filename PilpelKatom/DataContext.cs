9408368d8e04b5144e5c37a080760035bf4f7919
amirlevis <amir.levi.pro@gmail.com>
2023-08-29 01:40:19 +0300

    added token

--- PilpelKatom/DataContext.cs
+++ PilpelKatom/DataContext.cs
@@ -4,15 +4,8 @@ public class DataContext : DbContext
 {
     public DataContext(DbContextOptions<DataContext> options) : base(options)
     {
-        
     }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        base.OnConfiguring(optionsBuilder);
-        optionsBuilder.UseSqlServer("Server=localhost;Database=pilpel-katom;Trusted_Connection=true;TrustServerCertificate=true");
-    }
-
+    
     public DbSet<SuperHero> SuperHeroes => Set<SuperHero>();
     public DbSet<Character> Characters => Set<Character>();
     public DbSet<User> Users => Set<User>();
